32bbabeff4b0823868b7aacc1c3a105252e7ca2b
ojashparajuli <ojashparajuli@gmail.com>
2026-02-16 20:16:13 -0600

    added weather app code

---
+++ weather.py
@@ -0,0 +1,44 @@
+import requests
+import os
+
+from datetime import datetime
+
+api_key = "ee81424300ccd1d61a5b515831c9c5bc"
+   
+if not api_key:
+    raise SystemExit("Missing api key")
+
+
+r = requests.get("https://ipinfo.io/json")
+
+data = r.json()
+city=data["city"]
+
+params = {
+    "q" : city,
+    "appid" : api_key,
+    "units" : "imperial"
+}
+
+
+weather_url = "https://api.openweathermap.org/data/2.5/weather"
+weather_responce = requests.get(weather_url, params=params)
+
+
+weather_data = weather_responce.json()
+
+if weather_responce.status_code == 401:
+    print ("Invalid API Ojash do it right")
+
+if weather_responce.status_code == 200:
+    print("\nCurrent Weather\nCity: ", city,
+          "\nStatus: ", weather_data["weather"][0]["main"],
+          "\nStatus(id): ", weather_data["weather"][0]["id"],
+          "\nFeels Like: ", weather_data["main"]["feels_like"],
+          "\nTemperature: ", weather_data["main"]["temp"], "F",
+          "\nBase: ",weather_data["base"])
+    print ("Wind Speed: ", weather_data["wind"]["speed"])
+else:
+    print ("Error", weather_data)
+
+
