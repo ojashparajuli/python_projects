1db25c32df13a87f712c7a74ead202965d5f8b7f
ojashparajuli <ojashparajuli@gmail.com>
2026-02-18 22:03:43 -0600

    Initial commit - weather app

--- weather.py
+++ weather.py
@@ -1,16 +1,31 @@
 import requests
+<<<<<<< HEAD
+=======
+from dotenv import load_dotenv
+>>>>>>> d109180 (Initial commit - weather app)
 import os
 
 from datetime import datetime
 
+<<<<<<< HEAD
 api_key = "ee81424300ccd1d61a5b515831c9c5bc"
    
+=======
+load_dotenv()
+
+api_key = os.getenv("OPENWEATHER_API_KEY")
+
+>>>>>>> d109180 (Initial commit - weather app)
 if not api_key:
     raise SystemExit("Missing api key")
 
 
 r = requests.get("https://ipinfo.io/json")
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> d109180 (Initial commit - weather app)
 data = r.json()
 city=data["city"]
 
@@ -21,15 +36,30 @@ params = {
 }
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> d109180 (Initial commit - weather app)
 weather_url = "https://api.openweathermap.org/data/2.5/weather"
 weather_responce = requests.get(weather_url, params=params)
 
 
+<<<<<<< HEAD
 weather_data = weather_responce.json()
 
 if weather_responce.status_code == 401:
     print ("Invalid API Ojash do it right")
 
+=======
+
+weather_data = weather_responce.json()
+
+
+if weather_responce.status_code == 401:
+    print ("Invalid API Ojash do it right")
+
+
+>>>>>>> d109180 (Initial commit - weather app)
 if weather_responce.status_code == 200:
     print("\nCurrent Weather\nCity: ", city,
           "\nStatus: ", weather_data["weather"][0]["main"],
@@ -38,7 +68,14 @@ if weather_responce.status_code == 200:
           "\nTemperature: ", weather_data["main"]["temp"], "F",
           "\nBase: ",weather_data["base"])
     print ("Wind Speed: ", weather_data["wind"]["speed"])
+<<<<<<< HEAD
 else:
     print ("Error", weather_data)
 
 
+=======
+
+else:
+
+    print ("Error", weather_data)
+>>>>>>> d109180 (Initial commit - weather app)
